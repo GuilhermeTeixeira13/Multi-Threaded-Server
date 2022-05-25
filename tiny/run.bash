842782d1a5fdc9f493d4a1a49452b0bc2cd4b0ef
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2022-05-25 20:40:32 +0100

    video1 done

---
+++ tiny/run.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for N in {1..50}
+do
+  if [[ $N -gt 40 ]]
+  then
+    ./client localhost 8080 /file_example_MP4_480_1_5MG.mp4 &
+  else
+    ./client localhost 8080 /home.html &
+  fi
+done
+wait
