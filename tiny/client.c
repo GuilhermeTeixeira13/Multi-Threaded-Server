2d16b805643c4971c829c85e7ec2cdb83a926f2e
João Martins <joaobaltazar55@gmail.com>
2022-06-04 16:53:44 +0100

    CLIENT PROTOTIP

--- tiny/client.c
+++ tiny/client.c
@@ -19,6 +19,81 @@ Muitas Modificações
 
 #include "csapp.h"
 
+/* Function headers */
+int connect_and_send_request(int argc, char **argv, int fileNum, int policy);
+void *workerCONCURFunc(void *tInf);
+void *workerFIFOFunc(void *tInf);
+
+/*Globals and Defined variables*/
+
+// Policies
+#define CONCUR 1
+#define FIFO 2
+
+pthread_barrier_t mybarrier;
+pthread_cond_t condVerb = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t fifoMutex = PTHREAD_MUTEX_INITIALIZER;
+int fifoTurn;
+int numberOfThreads;
+
+/* Data structures*/
+typedef struct thread_information
+{
+    char **argv;
+    int argc;
+    int id;
+} thread_info;
+
+// Get host information (used to establishConnection)
+struct addrinfo *getHostInfo(char *host, char *port) {
+    int r;
+    struct addrinfo hints, *getaddrinfo_res;
+    // Setup hints
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    if ((r = getaddrinfo(host, port, &hints, &getaddrinfo_res))) {
+        fprintf(stderr, "[getHostInfo:21:getaddrinfo] %s\n", gai_strerror(r));
+        return NULL;
+    }
+
+    return getaddrinfo_res;
+}
+
+// Establish connection with host
+int establishConnection(struct addrinfo *info) {
+    if (info == NULL) return -1;
+
+    int clientfd;
+    for (; info != NULL; info = info->ai_next) {
+        if ((clientfd = socket(info->ai_family,
+                               info->ai_socktype,
+                               info->ai_protocol)) < 0) {
+            perror("[establishConnection:35:socket]");
+            continue;
+        }
+
+        if (connect(clientfd, info->ai_addr, info->ai_addrlen) < 0) {
+            close(clientfd);
+            perror("[establishConnection:42:connect]");
+            continue;
+        }
+
+        freeaddrinfo(info);
+        return clientfd;
+    }
+
+    freeaddrinfo(info);
+    return -1;
+}
+
+// Send GET request
+void GET(int clientfd, char *path) {
+    char req[1000] = {0};
+    sprintf(req, "GET %s HTTP/1.0\r\n\r\n", path);
+    send(clientfd, req, strlen(req), 0);
+}
+
 int main(int argc, char **argv)
 {
     char buffer[BUFSIZ];
@@ -29,7 +104,6 @@ int main(int argc, char **argv)
     char request[MAX_REQUEST_LEN];
     char request_template[] = "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n";
     struct protoent *protoent;
-    int 
 
     char *hostname = "localhost";
     unsigned short server_port = 8080; // default port
@@ -45,14 +119,20 @@ int main(int argc, char **argv)
     struct hostent *hostent;
     struct sockaddr_in sockaddr_in;
 
+    char *schedAlg;
+    int numThreads;
 
-    int numthreads;
-    char* schedAlg;
+
+
+     /* Checks for amount of arguments */
+    if (argc == 4) {
+        connect_and_send_request(argc, argv, 0, -1);
+    }
 
     /* Check command line args */
     if (argc != 6)
     {
-        fprintf(stderr, "usage: %s <host> <port> <threads> <schedalg> <filename1> <filename2>\n", argv[0]);
+        fprintf(stderr, "usage: %s <host> <port> <threads> <schedalg> <filename1> <filename2 - Optional>\n", argv[0]);
         exit(1);
     }
     if (argc > 1)
@@ -60,7 +140,7 @@ int main(int argc, char **argv)
     if (argc > 2)
         server_port = strtoul(argv[2], NULL, 10);
     if (argc > 3)
-        numthreads = atoi(argv[3]);
+        numThreads = atoi(argv[3]);
     if (argc > 4)
         /* getting scheduling policy*/
         schedAlg = argv[4];
@@ -70,25 +150,96 @@ int main(int argc, char **argv)
     else
         file = fileindex; // ou escolher outra filedynamic filestatic
 
-
-            
     int policy = -1;
+
+    if (strcmp(schedAlg, "CONCUR") == 0)
+        policy = CONCUR; // FIFO is any policy, after all
+    else if (strcmp(schedAlg, "FIFO") == 0)
+        policy = FIFO;
+    else
+    {
+        printf("Error: \"%s\" is Invalid scheduling policy, try CONCUR or FIFO\n", schedAlg);
+        return 1;
+    }
+
+    /* --Thread creation-- */
+    pthread_t threadIds[numThreads]; // holds the pthread_t for each thread
+    thread_info tInfo[numThreads];   // array of thread_info structs for each thread
+
+    int ret;
+
+    ret = pthread_barrier_init(&mybarrier, NULL, numThreads);
+
+    if (ret != 0)
+    {
+        printf("Could not create thread barrier\n");
+        return 1;
+    }
+
+    ret = pthread_cond_init(&condVerb, NULL);
+    
+    if(ret != 0){
+        printf("Could not create condition variable\n");
+      return 1;
+    }
+
+    numberOfThreads = numThreads;
+
+    int i;
+
+    if (policy == CONCUR) {
+        for (i = 0; i < numThreads; i++) {
+            //sets struct params
+            tInfo[i].id = i;
+            tInfo[i].argv = argv;
+            tInfo[i].argc = argc;
+
+            //creates a thread
+            ret = pthread_create(&threadIds[i], NULL, workerCONCURFunc, (void *) &tInfo[i]);
+
+            if(ret != 0){
+                printf("Could not create thread\n");
+                return 1;
+            }
+        }
+    }
+
+    if (policy == FIFO) {
+        fifoTurn = 0;
+
+        for (i = 0; i < numThreads; i++) {
+            //sets struct params
+            tInfo[i].id = i;
+            tInfo[i].argv = argv;
+            tInfo[i].argc = argc;
+
+            //creates a thread
+            ret = pthread_create(&threadIds[i], NULL, workerFIFOFunc, (void *) &tInfo[i]);
+
+            if (ret != 0) {
+                printf("Could not create thread\n");
+                return 1;
+            }
+        }
+    }
+
+    pthread_barrier_wait(&mybarrier);
+
+    pthread_exit(NULL);
+
+    return 0;
+
+
+
+
+
+
     // construção do pedido de http
     request_len = snprintf(request, MAX_REQUEST_LEN, request_template, file, hostname);
     if (request_len >= MAX_REQUEST_LEN)
     {
         fprintf(stderr, "request length large: %d\n", request_len);
         exit(EXIT_FAILURE);
-    }
-
-    if  (strcmp(schedAlg,"CONCUR") == 0) 
-            policy = CONCUR;//FIFO is any policy, after all
-    else if
-            (strcmp(schedAlg,"FIFO") == 0) 
-            policy = FIFO;
-    else{
-        printf("Error: \"%s\" is Invalid scheduling policy, try CONCUR or FIFO\n",schedAlg);
-        return 1;
     }
 
     /* Build the socket. */
@@ -164,4 +315,104 @@ int main(int argc, char **argv)
     Close(socket_file_descriptor);
 
     exit(EXIT_SUCCESS);
+}
+
+
+void *workerCONCURFunc (void *tInf) {
+    thread_info *tInfo = tInf;
+    int j = 0;
+
+    while (1) {
+        if (tInfo->argc == 6) {
+            connect_and_send_request(tInfo->argc, (char **) tInfo->argv, 0, tInfo->id);
+            connect_and_send_request(tInfo->argc, (char **) tInfo->argv, 1, tInfo->id);
+        }
+        else
+            connect_and_send_request(tInfo->argc, (char **) tInfo->argv, 0, -1);
+
+        pthread_barrier_wait(&mybarrier);
+        j++;
+    }
+    return 0;
+}
+
+void *workerFIFOFunc (void *tInf) {
+    thread_info *tInfo = tInf;
+    int j = 0;
+
+    while (1) {
+        if (tInfo->id == 0)
+           fifoTurn = 0;
+
+        pthread_mutex_lock( &fifoMutex);
+        while (tInfo->id != fifoTurn) {
+            pthread_cond_signal(&condVerb);
+            pthread_cond_wait( &condVerb, &fifoMutex);
+        }
+
+
+        /* WORK */
+        if (tInfo->argc == 6) {
+
+            connect_and_send_request(tInfo->argc, (char **) tInfo->argv, 0, tInfo->id);
+            connect_and_send_request(tInfo->argc, (char **) tInfo->argv, 1, tInfo->id);
+        }
+        else
+            connect_and_send_request(tInfo->argc, (char **) tInfo->argv, 0, tInfo->id);
+
+
+        pthread_barrier_wait(&mybarrier);
+        j++;
+    }
+    return 0;
+}
+
+int connect_and_send_request (int argc, char **argv, int fileNum, int policy) {
+    int clientfd;
+    char buf[BUFSIZ];
+
+    // Establish connection with <hostname>:<port>
+    clientfd = establishConnection(getHostInfo(argv[1], argv[2]));
+    if (clientfd == -1) {
+        fprintf(stderr,
+                "[main:73] Failed to connect to: %s:%s%s \n",
+                argv[1], argv[2], argv[3]);
+        return 3;
+    }
+
+    // Send GET request > stdout
+    if (policy >= 0) {
+//        printf("%d done fifo: %d\n", policy, fifoTurn);
+
+        if (fifoTurn == numberOfThreads-1)
+            fifoTurn = 0;
+        else
+            fifoTurn++;
+//        printf("%d\n", fifoTurn);
+
+        if (argc == 6)
+            GET(clientfd, argv[5]);
+        else {
+            GET(clientfd, argv[5 + fileNum]);
+        }
+
+        pthread_cond_signal(&condVerb);
+        pthread_mutex_unlock( &fifoMutex );
+    }
+    else {
+        if (argc == 6)
+            GET(clientfd, argv[5]);
+        else if (argc == 7) {
+            GET(clientfd, argv[5 + fileNum]);
+        } else
+            GET(clientfd, argv[3]);
+    }
+
+    while (recv(clientfd, buf, BUFSIZ, 0) > 0) {
+        fputs(buf, stdout);
+        memset(buf, 0, BUFSIZ);
+    }
+
+    close(clientfd);
+    return 0;
 }
